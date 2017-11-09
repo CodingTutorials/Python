4ec06eda8883b7cb4c48552892faf2cb2dcb0430
Nathan R <NJR4123@gmail.com>
2017-11-09 16:48:53 -0500

    Update numberguesser.py

--- Projects/numberguesser.py
+++ Projects/numberguesser.py
@@ -4,14 +4,16 @@ from random import *
 # Variables
 pasw = 1
 start = 1
+password = 0
 # Code
   
-password = randint(10000, 100000)
-print(password)
+# password = randint(10000, 100000)
+# print(password)
 
 # Easy|Meduim|Hard|Impossible
 choice = input("E|M|H|I:")
 
+# Easy
 if choice == "E":
   while pasw == 1:
     if start == 1:
@@ -30,4 +32,63 @@ if choice == "E":
     elif password != passwordguess:
       print("Wrong!")
       pasw = 1
-      
+
+# Meduim
+if choice == "M":
+  while pasw == 1:
+    if start == 1:
+      print(password)
+      start = 0
+    passwordguess = int(input("Password:"))
+    if password == passwordguess:
+      print("Correct!")
+      password = randint(1, 100)
+      print(password)
+      pasw = 1
+    elif password > passwordguess:
+      print("More")
+    elif password < passwordguess:
+      print("Less")
+    elif password != passwordguess:
+      print("Wrong!")
+      pasw = 1
+
+# Hard
+if choice == "H":
+  while pasw == 1:
+    if start == 1:
+      print(password)
+      start = 0
+    passwordguess = int(input("Password:"))
+    if password == passwordguess:
+      print("Correct!")
+      password = randint(1, 1000)
+      print(password)
+      pasw = 1
+    elif password > passwordguess:
+      print("More")
+    elif password < passwordguess:
+      print("Less")
+    elif password != passwordguess:
+      print("Wrong!")
+      pasw = 1      
+
+# Impossible
+if choice == "I":
+  while pasw == 1:
+    if start == 1:
+      print(password)
+      start = 0
+    passwordguess = int(input("Password:"))
+    if password == passwordguess:
+      print("Correct!")
+      password = randint(10000, 100000)
+      print(password)
+      pasw = 1
+    elif password > passwordguess:
+      print("More")
+    elif password < passwordguess:
+      print("Less")
+    elif password != passwordguess:
+      print("Wrong!")
+      pasw = 1
