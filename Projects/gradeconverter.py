bd1c963494637c40ff645a365feab1b776731f62
Nathan R <NJR4123@gmail.com>
2017-11-09 12:56:58 -0500

    Update gradeconverter.py

--- Projects/gradeconverter.py
+++ Projects/gradeconverter.py
@@ -1,51 +1,65 @@
 # Grade Converter
 # http://cs.smith.edu/~jorourke/Grading.html
-Choice = input("Fraction/Percent [F/P]:")
 
-if Choice == "F" or Choice == "f":
-  Numerator = float(input("Points: "))
-  Denominator = float(input("Total: "))
-  Score = Numerator/Denominator * 100
+# Checker
 
-if Choice == "P" or Choice == "p":
-  Score = float(input("Percent: "))
-
-
-if Score >= 97.5:
-  print("A+")
-
-elif Score >= 92.5:
-  print("A")
-
-elif Score >= 90.0:
-  print("A-")
-
-elif Score >= 87.5:
-  print("B+")
-
-elif Score >= 82.5:
-  print("B")
-
-elif Score >= 80.0:
-  print("B-")
-
-elif Score >= 77.5:
-  print("C+")
-
-elif Score >= 72.5:
-  print("C")
-
-elif Score >= 70.0:
-  print("C-")
-
-elif Score >= 67.5:
-  print("D+")
-
-elif Score >= 62.5:
-  print("D")
+def checker():
+  if Score >= 97.5:
+    print("A+")
   
-elif Score >= 60.0:
-  print("D-")
+  elif Score >= 92.5:
+    print("A")
   
-elif Score < 60.0:
-  print("F")
+  elif Score >= 90.0:
+    print("A-")
+  
+  elif Score >= 87.5:
+    print("B+")
+  
+  elif Score >= 82.5:
+    print("B")
+  
+  elif Score >= 80.0:
+    print("B-")
+  
+  elif Score >= 77.5:
+    print("C+")
+  
+  elif Score >= 72.5:
+    print("C")
+  
+  elif Score >= 70.0:
+    print("C-")
+  
+  elif Score >= 67.5:
+    print("D+")
+  
+  elif Score >= 62.5:
+    print("D")
+    
+  elif Score >= 60.0:
+    print("D-")
+    
+  elif Score < 60.0:
+    print("F")
+
+# End Of Checker
+
+ask = 1
+
+while ask == 1:
+  Choice = input("Fraction/Percent [F/P]:")
+  ask = 0
+
+  if Choice == "F" or Choice == "f":
+    Numerator = float(input("Points: "))
+    Denominator = float(input("Total: "))
+    Score = Numerator/Denominator * 100
+    checker()
+  
+  if Choice == "P" or Choice == "p":
+    Score = float(input("Percent: "))
+    checker()
+  
+  else:
+    ask = 1
