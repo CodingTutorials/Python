ab1e88fb10baa631e42cf8c41ab42f3ae9a9b201
Nathan R <NJR4123@gmail.com>
2017-11-10 14:18:43 -0500

    Create PyCmd2.0.py

---
+++ Projects/PyCmd2.0.py
@@ -0,0 +1,36 @@
+# Imports
+from random import *
+
+# Variables
+adminuser = "Admin"
+adminpass = "Admin"
+
+# Define
+
+# Console
+def console():
+  command = input(":")
+
+# Login
+def login():
+  username = input("Username: ")
+  password = input("Password: ")
+  if username == "Admin" and password == "Admin":
+    print("Authentication Success")
+    
+  elif username == "Guest" and password == "Guest":
+    print("Authentication Success")
+    
+  else:
+    print("Authentication Failed")
+    
+# Number Generator    
+def numgen():
+  number = randint(1, 10)
+
+  
+# Code
+print("Welcome to PyCmd 2.0!")
+loginchoice = input("Would you like to login?")
+
+
