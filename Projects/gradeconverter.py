ab4a6f3b2c26d8ded39f46b013b636b832e18901
Nathan R <NJR4123@gmail.com>
2017-11-13 21:22:37 -0500

    Update gradeconverter.py

--- Projects/gradeconverter.py
+++ Projects/gradeconverter.py
@@ -57,12 +57,12 @@ def checker():
 text = "Welcome to Grade Converter"
 for character in text:
   sys.stdout.write(character)
-  sleep(0.1)
+  sleep(0.05)
 print("\n")
 text = "Created by Nathan R (Mosrod)"
 for character in text:
   sys.stdout.write(character)
-  sleep(0.05)
+  sleep(0.03)
 print("\n")
 
 while ask == 1:
