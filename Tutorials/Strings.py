86fb7441042a38ee14f97dea035d39e76bf47975
Nathan R <NJR4123@gmail.com>
2019-10-01 14:51:21 -0400

    Updated Python string tutorial documentation

--- Tutorials/Strings.py
+++ Tutorials/Strings.py
@@ -2,3 +2,17 @@
 
 # Print welcome message
 print("Hello World")
+
+# Strings can either be surrounded in " or '
+print('Hello World')
+
+# You can use variables to store strings, then print them out
+message = "Hello World"
+print(message)
+
+# If you want to put single quotes in a single quote string or double quotes in a double quote string, you need to change the string's quote type
+# Invalid: 'hello's world'
+message = "hello's world"  # Correct
+
+# Invalid: "he said "welcome!""
+message = 'he said "welcome!"'  # Correct
